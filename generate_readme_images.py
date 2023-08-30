e9f30c09f2b68b4b08f4e4c73119d0127d5020b1
abidshahriar <64680295+abidshahriar@users.noreply.github.com>
2023-08-30 17:35:02 +0600

    generate_readme_images.py

--- generate_readme_images.py
+++ generate_readme_images.py
@@ -8,13 +8,13 @@ def generate_image_markdown(num_cols, image_dir):
     # Get a sorted list of image files in the directory
     images = sorted(os.listdir(image_dir))
     num_images = len(images)
-    
+
     # Calculate the number of rows needed for the table
     num_rows = (num_images + num_cols - 1) // num_cols
 
     # Populate the table with image links using a ternary operator
     markdown += "".join(
-        f"| ![Image](/{image_dir}/{images[i * num_cols + j]}) |" if i * num_cols + j < num_images
+        f"| ![Image]({image_dir}/{images[i * num_cols + j]}) |" if i * num_cols + j < num_images
         else " |"
         for i in range(num_rows)
         for j in range(num_cols)
@@ -27,6 +27,22 @@ if __name__ == "__main__":
     image_dir = "pcb_images"
     markdown = generate_image_markdown(num_cols, image_dir)
 
-    # Write the markdown to a file
-    with open("test.md", "w") as f:
-        f.write(f"# My Project\n\n{markdown}")
+    # Read the existing README.md
+    with open("README.md", "r") as f:
+        readme_content = f.read()
+
+    # Find the start and end markers for the pictures section
+    start_marker = "## Pictures"
+    end_marker = "|  |  |  |  |"
+
+    # Replace the existing pictures section with the updated one
+    new_readme_content = re.sub(
+        f"{start_marker}.*?{end_marker}",
+        f"{start_marker}\n\n{markdown}\n\n{end_marker}",
+        readme_content,
+        flags=re.DOTALL,
+    )
+
+    # Update the README.md file
+    with open("README.md", "w") as f:
+        f.write(new_readme_content)
